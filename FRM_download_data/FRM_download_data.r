b0d3aac0430288e7706b35969118c83b53d62ccd
Lining Yu <yulining@hu-berlin.de>
2016-01-20 10:58:28 +0100

    Update companylist 2016

--- FRM_download_data/FRM_download_data.r
+++ FRM_download_data/FRM_download_data.r
@@ -14,9 +14,9 @@ lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 # setwd("//clapton.wiwi.hu-berlin.de/frm/codes")
 
 # for symbols in Yahoo finance
-companylist = read.csv("companylist 2015.csv")
+companylist = read.csv("companylist 2016.csv")
 
-# companylist2015 - downloaded from this source
+# companylist2016 - downloaded from this source
 # http://www.nasdaq.com/screening/companies-by-industry.aspx?industry=Finance&sortname=country&sorttype=1
 
 # Array with firm names sorterd by market capitalization
